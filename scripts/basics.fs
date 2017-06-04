365b237f856cd356c4bef66f56648c58736cea38
Scott Ranger <millenniumnight@googlemail.com>
2017-06-04 22:38:34 +0100

    Update basics.fs

--- scripts/basics.fs
+++ scripts/basics.fs
@@ -3,6 +3,10 @@ printfn "Hello %s" "Bar"
 printfn "Hello %i" 42
 
 let square num = num * num
-let squared = List.map square [1;2;3;4;5]
+let squared = List.map square [2;3;4;5]
 
 printfn "%A" squared
+
+let updatedSquared = square 1 :: squared
+
+printfn "%A" updatedSquared
